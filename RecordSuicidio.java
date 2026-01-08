863609332a0aa3672b37e695c682d46138ccacdb
BELLAFKIH.22128 <bellafkih.deyae.studente@itispaleocapa.it>
2026-01-08 08:33:17 +0100

    Classe Record dei Suicidi

---
+++ RecordSuicidio.java
@@ -0,0 +1,31 @@
+public class RecordSuicidio {
+    private String[] campi;
+    private int mioValore;
+    private boolean cancellato;
+
+    public RecordSuicidio(String[] campi, int mioValore) {
+        this.campi = campi;
+        this.mioValore = mioValore;
+        this.cancellato = false;
+    }
+
+    public String[] getCampiCompleti() {
+        String[] risultato = new String[campi.length + 2];
+        System.arraycopy(campi, 0, risultato, 0, campi.length);
+        risultato[risultato.length - 2] = String.valueOf(mioValore);
+        risultato[risultato.length - 1] = String.valueOf(cancellato);
+        return risultato;
+    }
+
+    public String getChiave() {
+        return campi[0];
+    }
+
+    public void modificaCampo(int indice, String valore) {
+        campi[indice] = valore;
+    }
+
+    public void cancellaLogicamente() {
+        cancellato = true;
+    }
+}
